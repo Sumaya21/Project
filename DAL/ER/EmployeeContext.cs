ac9489ae00907c2d5e922cee072fbd188017091f
Sumaya21 <sumayajahankanta@gmail.com>
2023-12-16 02:04:27 +0600

    Add project files.

---
+++ DAL/ER/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using DAL.ER.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.ER
+{
+    public class EmployeeContext:DbContext
+    {
+        public DbSet<Employee>Employees { get; set; }   
+    }
+}
